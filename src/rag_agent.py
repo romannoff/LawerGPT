53493f3982d3eb28e0695f5965a4bf10402aa2fb
RomanAf <>
2025-06-12 14:31:59 +0300

    add parameters modification

--- src/rag_agent.py
+++ src/rag_agent.py
@@ -8,76 +8,109 @@ from database.history_base import HistoryBase
 from src.agent import Agent, AgentAnswer
 from database.id_to_str import codes_id_to_str, laws_id_to_str
 from src.prompts import RagPrompts, SummarizePrompt, RouterPrompt
+from src.logging_conf import logger
+from src.config import Config
+
+
+settings = Config.from_yaml("config.yaml")
+
 
 class RagAgent(Agent):
-    def __init__(self):
+    def __init__(self, 
+                answer_temperature: float=0.7,
+                router_temperature: float=0.2,
+                reranker_temperature: float=0.7, 
+                chunk_count: int=10,
+                reranker_enable: bool=True,
+                router_enable: bool=True,
+                db_enable: bool=True,
+                is_test: bool=False,
+                chain_of_thoughts: bool=False,
+                chat_id: str='0',
+                mode: str='standard'
+                ):
         self.database = QdrantLegalRAG()
         self.history_base = HistoryBase()
 
+        self.answer_temperature = answer_temperature
+        self.router_temperature = router_temperature
+        self.reranker_temperature = reranker_temperature
+        self.reranker_enable = reranker_enable
+        self.router_enable = router_enable
+        self.db_enable = db_enable
+        self.chunk_count = chunk_count
+        self.is_test = is_test
+        self.chain_of_thoughts = chain_of_thoughts
+        self.chat_id = chat_id
+
         self.llm_model = ChatOpenAI(
-            base_url="http://192.168.1.93:2727/v1",
-            model='Qwen2.5-7B-AWQ',
-            openai_api_key='password',
-            temperature=0.7,
-            top_p=0.9,
+            base_url=settings.base_url,
+            model=settings.model,
+            openai_api_key=settings.password,
+            temperature=self.answer_temperature,
             stop_sequences=["<|im_end|>", "<|im_start|>", "<|eot_id|>"],
         )
 
         self.so_llm_model = OpenAI(
-            base_url="http://192.168.1.93:2727/v1", 
-            api_key="password",
-            )
+            base_url=settings.base_url,
+            api_key=settings.password,
+        )
 
         self.final_prompt = RagPrompts().simple_prompt
-
         self.summaryze_prompts = SummarizePrompt()
         self.summarize_prompt = self.summaryze_prompts.prompt
         self.reranker_prompt = self.summaryze_prompts.reranker_prompt
-        
         self.router_prompt = RouterPrompt().prompt
-        
         self.summarize_articles = True
 
+    def set_settings(self, **kwargs):
+        self.answer_temperature = kwargs.get('answer_temperature', self.answer_temperature)
+        self.router_temperature = kwargs.get('router_temperature', self.router_temperature)
+        self.reranker_temperature = kwargs.get('reranker_temperature', self.reranker_temperature)
+        self.reranker_enable = kwargs.get('reranker_enable', self.reranker_enable)
+        self.router_enable = kwargs.get('router_enable', self.router_enable)
+        self.db_enable = kwargs.get('db_enable', self.db_enable)
+        self.chunk_count = kwargs.get('chunk_count', self.chunk_count)
+        self.is_test = kwargs.get('is_test', self.is_test)
+        self.chain_of_thoughts = kwargs.get('chain_of_thoughts', self.chain_of_thoughts)
+        self.chat_id = kwargs.get('chat_id', self.chat_id)
+
     def get_article_text(self, articles: list[dict], article_type: str):
+        """
+        Преобразует полученные чанки в текст для промпта
+        """
         if not articles:
             return ''
         result = 'Конституция РФ:\n\n' if article_type == 'constitution' else ''
         for article in articles:
-            # Заголовок
             if article.get('title') is not None:
                 result += article['title']
             else:
                 result += self.get_title(article, article_type)
-
-            # Текст
-            if article.get('text') is not None:
-                result += article['text'] + '\n\n'
-            elif article.get('summarize') is not None:
-                result += article['summarize'] + '\n\n'
+            result += article['text'] + '\n\n'
         return result
-
 
     @staticmethod
     def get_title(article, article_type):
+        """
+        Получает точное название статьи
+        """
         if article_type == 'constitution':
             return article['num'] + '\n'
-        
         if article_type == 'code':
             result = codes_id_to_str(article['codes_id']) + '\n'
-            
         elif article_type == 'law':
             result = laws_id_to_str(article['law_id']) + '\n'
-
         result += article['chapter_num'] + ' ' + article['article_title'] + '. ' + article['article_num'] + '\n'
         return result
 
     def summarize(self, query, articles, article_type):
-
+        """
+        Суммаризирует текст статьи, выделяя только нужные фрагменты
+        """
         for article in articles:
             text = self.get_title(article, article_type=article_type) + article['text']
-
             prompt = self.summarize_prompt(query, text)
-
             summarize_text = self.llm_model.invoke(prompt).content
             if summarize_text.startswith('В данной статье нет информации по запросу'):
                 article['text'] = ''
@@ -85,101 +118,123 @@ class RagAgent(Agent):
                 article['text'] = summarize_text
 
     def reranker(self, query, articles, article_type):
+        """
+        Суммаризирует текст списка статей, выбирая только нужную информацию.
+        Помимо этого присуждает каждой статье оценку:
+        0 - статья не подходит под вопрос,
+        2 - статья частично подходит под вопрос,
+        4 - статья полностью подходит под вопрос 
+        """
+        if not articles:
+            return []
+
         filtered_articles = []
         for article in articles:
-            text = self.get_title(article, article_type=article_type) + article['text']
-
+            title = self.get_title(article, article_type=article_type)
+            text = title + article['text']
             prompt, extra_body = self.reranker_prompt(query, text)
-
             completion = self.so_llm_model.chat.completions.create(
-                model='Qwen2.5-7B-AWQ',
+                model=settings.model,
                 messages=prompt,
                 extra_body=extra_body,
-                temperature=0.7,
+                temperature=self.reranker_temperature,
                 max_tokens=10_000,
             )
             res = json.loads(completion.choices[0].message.content)
-            if int(res['estimation']) > 0:
-                res['title'] = self.get_title(article, article_type)
-                filtered_articles.append(res)
-        
-        return sorted(filtered_articles, key=lambda x: int(x['estimation']), reverse=True)
+
+            logger.info('RERANKER: %s\nSCORE: %s\nTEXT:%s\n\n' % (title, res['estimation'], res['summarize']))
+            
+            if not self.reranker_enable:
+                filtered_articles.append({
+                    'title': title,
+                    'text': res['summarize'],
+                    'relevance_score': 0
+                })
+            
+            elif int(res['estimation']) > 0:
+                filtered_articles.append({
+                    'title': title,
+                    'text': res['summarize'],
+                    'relevance_score': int(res['estimation'])
+                })
+        return sorted(filtered_articles, key=lambda x: x['relevance_score'], reverse=True)
 
     def router(self, query):
-        # messages = self.history_base.get_messages(chat_id=0, limit=8)
-        messages = None
+        """
+        Роутер, переформулирующий вопрос, исходя из истории, если вопрос качается юридической темы,
+        иначе даёт пользователю ответ средствами LLM.
+        """
+        if not self.router_enable:
+            return 'legal', query
+        if not self.is_test:
+            messages = self.history_base.get_messages(chat_id=self.chat_id, limit=4)
+        else:
+            messages = []
 
         prompt, extra_body = self.router_prompt(query, messages)
         completion = self.so_llm_model.chat.completions.create(
-                model='Qwen2.5-7B-AWQ',
-                messages=prompt,
-                extra_body=extra_body,
-                temperature=0.2,
-                max_tokens=500,
+            model=settings.model,
+            messages=prompt,
+            extra_body=extra_body,
+            temperature=self.answer_temperature,
+            max_tokens=500,
         )
-        # print(completion.choices[0].message.content)
-        answer = completion.choices[0].message.content
+        answer = completion.choices[0].message.content  
+
         try:
             res = json.loads(answer)
         except json.decoder.JSONDecodeError:
+            logger.warning('ROUTER: JSONDecodeError\nRESULT: %s\n\n' % (answer, ))
             if 'non_legal' in answer:
                 return 'non_legal', 'Я готов ответить на ваши юридические вопросы'
             return 'legal', query
-
+        
+        logger.info('ROUTER:\nTYPE: %s\nNEW QUESTION: %s\n\n' % (res['msg_type'], res['response']))
+        
         return res['msg_type'], res['response']
 
     def __call__(self, query):
+        """
+        Получение ответа от RAG на запрос пользователя
+        """
+
         msg_type, response = self.router(query)
-
-        self.history_base.add_message(role='user', text=query, chat_id=0)
-
-        if msg_type == 'non_legal':
-            self.history_base.add_message(role='assistant', text=response, chat_id=0)
-            return AgentAnswer(
-                    query=query,
-                    context=[],
-                    answer=response,
-                )
+        if not self.is_test:
+            self.history_base.add_message(role='user', text=query, chat_id=self.chat_id)
+        if msg_type == 'non_legal' and not self.is_test:
+            self.history_base.add_message(role='assistant', text=response, chat_id=self.chat_id)
+            return {'answer': response, 'contexts': []}
 
         query = response
-
-        # Получаем чанки из баз
-        constitutions = self.database.search_constitution_by_chapter(query, chapter_id=None, limit=10)
-        codes = self.database.search_codes_by_id(query, codes_id=None, limit=10)
-        laws = self.database.search_laws_by_id(query, law_id=None, limit=10)
+        if self.db_enable:
+            limit = self.chunk_count
+            constitutions = self.database.search_constitution_by_chapter(query, chapter_id=None, limit=limit)
+            codes = self.database.search_codes_by_id(query, codes_id=None, limit=limit)
+            laws = self.database.search_laws_by_id(query, law_id=None, limit=limit)
+        else:
+            constitutions = []
+            codes = []
+            laws = []
 
         constitutions = self.reranker(query, constitutions, article_type='constitution')
         codes = self.reranker(query, codes, article_type='code')
         laws = self.reranker(query, laws, article_type='law')
-
-        # self.summarize(query, constitutions, article_type='constitution')
-        # self.summarize(query, codes, article_type='code')
-        # self.summarize(query, laws, article_type='law')
+        
+        contexts = constitutions + codes + laws
 
         constitution_str = self.get_article_text(constitutions, article_type='constitution')
         codes_str = self.get_article_text(codes, article_type='code')
         law_str = self.get_article_text(laws, article_type='law')
 
-        prompt = self.final_prompt(
-            query, 
-            constitution_str,
-            codes_str,
-            law_str,
-            )
-
+        prompt = self.final_prompt(query, constitution_str, codes_str, law_str)
+        # print(prompt)
         answer = self.llm_model.invoke(prompt).content
 
-        self.history_base.add_message(role='assistant', text=answer, chat_id=0)
-        
-        return AgentAnswer(
-            query=query,
-            context=[constitution_str, codes_str, law_str],
-            answer=answer,
-        )
-
-        
-        
-        
+        if not self.is_test:
+            self.history_base.add_message(role='assistant', text=answer, chat_id=self.chat_id)
+        print(contexts)
+        return {'answer': '\n' + answer, 'contexts': contexts}
+    
 
 if __name__ == '__main__':
     s = RagAgent()
@@ -200,6 +255,4 @@ if __name__ == '__main__':
     print('_'*30)
 
     print(s('А если я был в состоянии аффекта?')['answer'])
-
-
-
+    
--- src/streamlit_ui.py
+++ src/streamlit_ui.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import uuid
 from datetime import datetime
+import pickle
+import os
+import json
+
+from rag_agent import RagAgent
+from database.history_base import HistoryBase
+from src.logging_conf import logger
+from src.config import Config
+
+
+settings = Config.from_yaml("config.yaml")
+
 
 # Настройка страницы
 st.set_page_config(
@@ -10,92 +22,112 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+
+def save_settings():
+    with open('settings.json', 'w') as f:
+        json.dump(st.session_state.settings, f)
+
+
+def load_settings():
+    if os.path.exists('settings.json'):
+        with open('settings.json', 'r') as f:
+            return json.load(f)
+    return settings.rag_settings.copy()
+
+def update_settings():
+    """Обновляет настройки из виджетов и пересоздает агента при необходимости"""
+    old_settings = st.session_state.settings.copy()
+    
+    # Обновляем настройки из виджетов
+    for key in settings.rag_settings.keys():
+        if key in st.session_state:
+            st.session_state.settings[key] = st.session_state[key]
+    
+    # Проверяем изменения и обновляем агента
+    if old_settings != st.session_state.settings:
+        st.session_state.rag_agent.set_settings(**st.session_state.settings)
+        logger.info(f"Settings updated: {st.session_state.settings}")
+
+
+
 # Инициализация session state
 if 'user_input' not in st.session_state:
     st.session_state['user_input'] = ""
 if 'chats' not in st.session_state:
-    st.session_state.chats = {}
+    if os.path.exists('chats.pl'):
+        with open('chats.pl', 'rb') as f:
+            st.session_state.chats = pickle.load(f)
+    else:
+        st.session_state.chats = {}
 if 'current_chat_id' not in st.session_state:
     st.session_state.current_chat_id = None
 if 'settings' not in st.session_state:
-    st.session_state.settings = {
-        'mode': 'standard',
-        'router_enabled': True,
-        'router_temperature': 0.7,
-        'db_enabled': True,
-        'chunks_count': 5,
-        'reranker_enabled': True,
-        'reranker_temperature': 0.5,
-        'chain_of_thoughts': False,
-        'multi_agent': False
-    }
+    st.session_state.settings = load_settings()
+if 'rag_agent' not in st.session_state:
+    st.session_state.rag_agent = RagAgent(**st.session_state.settings)
+
+if 'history_db' not in st.session_state:
+    st.session_state.history_db = HistoryBase()
 
 # Стили CSS
 st.markdown("""
 <style>
-    .chat-row {
-        display: flex;
-        width: 100%;
-        margin-bottom: 0.5rem;
-    }
-    .chat-row.user {
-        justify-content: flex-end;
-    }
-    .chat-row.agent {
-        justify-content: flex-start;
-    }
-    .chat-bubble {
-        max-width: 70%;
-        padding: 0.75rem 1rem;
-        border-radius: 1rem;
-        position: relative;
-        line-height: 1.4;
-    }
-    .chat-bubble.user {
-        background-color: #acf;
-        color: #000;
-        border-bottom-right-radius: 0;
-    }
-    .chat-bubble.agent {
-        background-color: #eee;
-        color: #000;
-        border-bottom-left-radius: 0;
-    }
+.chat-row { display: flex; width: 100%; margin-bottom: 0.5rem; }
+.chat-row.user { justify-content: flex-end; }
+.chat-row.agent { justify-content: flex-start; }
+.chat-bubble {
+    max-width: 70%;
+    padding: 0.75rem 1rem;
+    border-radius: 1rem;
+    position: relative;
+    line-height: 1.4;
+}
+.chat-bubble.user {
+    background-color: #acf;
+    color: #000;
+    border-bottom-right-radius: 0;
+}
+.chat-bubble.agent {
+    background-color: #eee;
+    color: #000;
+    border-bottom-left-radius: 0;
+}
+textarea.custom-textarea {
+    /* Дополнительные стили для textarea, если нужно */
+}
 </style>
 """, unsafe_allow_html=True)
+
+def dump_chats():
+    with open('chats.pl', 'wb') as f:
+        pickle.dump(st.session_state.chats, f)
 
 # Функции для работы с чатами
 def create_new_chat():
     chat_id = str(uuid.uuid4())
+    logger.info("NEW CHAT: %s" % (chat_id,))
     st.session_state.chats[chat_id] = {
         'id': chat_id,
         'name': f"Чат {len(st.session_state.chats) + 1}",
         'created_at': datetime.now(),
         'messages': []
     }
-    st.session_state.current_chat_id = chat_id
+    st.session_state.chat_id = chat_id
+    st.session_state.settings['chat_id'] = chat_id
+    dump_chats()
 
 def delete_chat(chat_id):
     if chat_id in st.session_state.chats:
-        del st.session_state.chats[chat_id]
-        if st.session_state.current_chat_id == chat_id:
-            st.session_state.current_chat_id = None
+        if st.session_state.current_chat_id != chat_id:
+            del st.session_state.chats[chat_id]
+            st.session_state.history_db.delete_messages_by_chat_id(chat_id)
+            dump_chats()
 
 def clear_chat_history():
     cid = st.session_state.current_chat_id
     if cid:
         st.session_state.chats[cid]['messages'] = []
-
-# Заглушка RAG
-def get_rag_response(query, settings):
-    return {
-        'answer': f"Ответ на ваш юридический вопрос: '{query}' ...",
-        'contexts': [
-            {'title': 'ГК РФ, ст.123', 'content': 'Пример...', 'relevance_score': 0.95},
-            {'title': 'Постановление Пленума ВС РФ, п.15', 'content': 'Пример...', 'relevance_score': 0.87},
-            {'title': 'ФЗ-123, ст.45', 'content': 'Пример...', 'relevance_score': 0.82},
-        ]
-    }
+        st.session_state.history_db.delete_messages_by_chat_id(chat_id)
 
 # Функция обработки ввода
 def process_input():
@@ -103,34 +135,31 @@ def process_input():
     if q:
         current_chat = st.session_state.chats.get(st.session_state.current_chat_id)
         if current_chat:
-            # Добавляем сообщение пользователя
             current_chat['messages'].append({
                 'role': 'user',
                 'content': q,
                 'timestamp': datetime.now()
             })
-            # Получаем ответ
             with st.spinner("Ищем ответ..."):
-                resp = get_rag_response(q, st.session_state.settings)
-            # Добавляем ответ ассистента
+                resp = st.session_state.rag_agent(q)
             current_chat['messages'].append({
                 'role': 'assistant',
                 'content': resp['answer'],
                 'contexts': resp['contexts'],
                 'timestamp': datetime.now()
             })
-            # Очищаем поле ввода
             st.session_state['user_input'] = ""
 
 # Левая боковая панель
 with st.sidebar:
     if st.button("➕ Новый чат", use_container_width=True):
-        create_new_chat()
+        if st.session_state.chats == {}:
+            create_new_chat()
         st.rerun()
 
     st.markdown("---")
     for chat_id, chat in st.session_state.chats.items():
-        cols = st.columns([8,1])
+        cols = st.columns([8, 1])
         with cols[0]:
             if st.button(chat['name'],
                          key=f"chat_{chat_id}",
@@ -139,9 +168,7 @@ with st.sidebar:
                 st.session_state.current_chat_id = chat_id
                 st.rerun()
         with cols[1]:
-            if st.button("🗑️",
-                         key=f"del_{chat_id}",
-                         use_container_width=True):
+            if st.button("🗑️", key=f"del_{chat_id}", use_container_width=True):
                 delete_chat(chat_id)
                 st.rerun()
 
@@ -151,37 +178,56 @@ col_main, col_settings = st.columns([3, 1])
 with col_main:
     st.markdown("## ⚖️ Юридическая RAG система")
 
-    # Выбор или создание чата
     if not st.session_state.current_chat_id:
         if not st.session_state.chats:
             create_new_chat()
         else:
             st.session_state.current_chat_id = next(iter(st.session_state.chats))
+
     current_chat = st.session_state.chats.get(st.session_state.current_chat_id)
 
     if current_chat:
-        # Кнопка очистки истории
         if st.button("🧹 Очистить диалог", use_container_width=True):
             clear_chat_history()
 
-        # Отображение истории чата
         for msg in current_chat['messages']:
             role = msg['role']
             content = msg['content']
             row_cls = "chat-row user" if role == "user" else "chat-row agent"
             bub_cls = "chat-bubble user" if role == "user" else "chat-bubble agent"
             st.markdown(f"""
-                <div class="{row_cls}">
-                <div class="{bub_cls}">
-                    {content}
-                </div>
-                </div>
-            """, unsafe_allow_html=True)
+<div class="{row_cls}">
+    <div class="{bub_cls}">
+        {content}
+    </div>
+</div>
+""", unsafe_allow_html=True)
 
-        # Поле ввода и кнопка отправки
-        st.text_area("Ваш вопрос:", height=100, key="user_input")
-        if st.button("Отправить", on_click=process_input, use_container_width=True):
-            pass
+            if role == 'assistant' and 'contexts' in msg:
+                for ctx in msg['contexts']:
+                    if ctx:  # Проверяем, что контекст не пустой
+                        with st.expander(f"{ctx['title']}"):
+                            st.markdown(ctx['text'])
+
+    st.text_area("Ваш вопрос:", height=100, key="user_input", help="Нажмите Ctrl+Enter для отправки")
+    st.button("Отправить", on_click=process_input, use_container_width=True, key="submit_button")
+
+    st.markdown("""
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const textarea = document.querySelector('textarea');
+    const submitButton = document.querySelector('button[data-testid="stButton"][id*="submit_button"]');
+    if (textarea && submitButton) {
+        textarea.addEventListener('keydown', function(event) {
+            if (event.ctrlKey && event.key === 'Enter') {
+                event.preventDefault();
+                submitButton.click();
+            }
+        });
+    }
+});
+</script>
+""", unsafe_allow_html=True)
 
 with col_settings:
     st.markdown("### ⚙️ Настройки")
@@ -190,10 +236,15 @@ with col_settings:
         mode = st.radio(
             "Режим работы:",
             options=['fast', 'standard', 'deep'],
-            format_func=lambda x: {'fast': '⚡ Быстрый', 'standard': '⚖️ Стандартный', 'deep': '🧠 Глубокий'}[x],
+            format_func=lambda x: {
+                'fast': '⚡ Быстрый',
+                'standard': '⚖️ Стандартный',
+                'deep': '🧠 Глубокий'
+            }[x],
             index=['fast', 'standard', 'deep'].index(st.session_state.settings['mode']),
             horizontal=True,
-            key="mode_selector"  # Добавляем уникальный ключ для радиокнопок
+            key="mode",
+            on_change=update_settings,
         )
         if mode != st.session_state.settings['mode']:
             st.session_state.settings['mode'] = mode
@@ -201,37 +252,46 @@ with col_settings:
 
     with st.expander("Настройки разработчика", expanded=False):
         st.checkbox("Включить роутер",
-                    value=st.session_state.settings['router_enabled'],
-                    key="router_enabled")
-        if st.session_state.settings['router_enabled']:
+                    value=st.session_state.settings['router_enable'],
+                    key="router_enable",
+                    on_change=update_settings
+                    )
+        if st.session_state.settings['router_enable']:
             st.slider("Температура роутера",
-                      0.0,1.0,st.session_state.settings['router_temperature'],step=0.1,
-                      key="router_temperature")
+                      0.0, 1.0, st.session_state.settings['router_temperature'], step=0.1,
+                      key="router_temperature",
+                      on_change=update_settings)
 
         st.markdown("---")
         st.checkbox("Включить БД",
-                    value=st.session_state.settings['db_enabled'],
-                    key="db_enabled")
-        if st.session_state.settings['db_enabled']:
-            st.number_input("Число чанков",1,20,
-                            st.session_state.settings['chunks_count'],
-                            key="chunks_count")
+                    value=st.session_state.settings['db_enable'],
+                    key="db_enable",
+                    on_change=update_settings)
+        if st.session_state.settings['db_enable']:
+            st.number_input("Число чанков", 1, 20,
+                            st.session_state.settings['chunk_count'],
+                            key="chunk_count",
+                            on_change=update_settings)
 
         st.markdown("---")
         st.checkbox("Включить реранкер",
-                    value=st.session_state.settings['reranker_enabled'],
-                    key="reranker_enabled")
-        if st.session_state.settings['reranker_enabled']:
+                    value=st.session_state.settings['reranker_enable'],
+                    key="reranker_enable",
+                    on_change=update_settings)
+        if st.session_state.settings['reranker_enable']:
             st.slider("Температура реранкера",
-                      0.0,1.0,st.session_state.settings['reranker_temperature'],step=0.1,
-                      key="reranker_temperature")
+                      0.0, 1.0, st.session_state.settings['reranker_temperature'], step=0.1,
+                      key="reranker_temperature",
+                      on_change=update_settings)
 
         st.markdown("---")
-        st.checkbox("Chain of Thoughts", key="chain_of_thoughts")
-        st.checkbox("Мультиагентный режим", key="multi_agent")
+        st.checkbox("Chain of Thoughts", key="chain_of_thoughts", on_change=update_settings)
 
     if st.button("💾 Сохранить настройки", use_container_width=True):
+        save_settings()
         st.success("Настройки сохранены!")
+
+
 
 # Футер
 st.markdown("---")
