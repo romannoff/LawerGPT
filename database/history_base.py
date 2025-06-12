27246b89fbf0547939ee7bc61563ea7ea619e74e
RomanAf <>
2025-06-12 14:29:15 +0300

    bug fix

--- database/history_base.py
+++ database/history_base.py
@@ -4,6 +4,9 @@ from sqlalchemy import (
 )
 from sqlalchemy import delete 
 
+from src.logging_conf import logger
+
+
 class HistoryBase:
     def __init__(self):
         database_url = "postgresql+psycopg2://legal_user:secret_password@localhost:5432/legal_db"
@@ -17,7 +20,7 @@ class HistoryBase:
         self.history = Table(
             'history', self.metadata,
             Column('id', Integer, primary_key=True),
-            Column('chat_id', Integer, ),
+            Column('chat_id', String),
             Column('role', String),
             Column('text', String),
             schema=schema
@@ -25,7 +28,7 @@ class HistoryBase:
 
         self.metadata.create_all(self.engine)
 
-    def add_message(self, chat_id: int, role: str, text: str):
+    def add_message(self, chat_id: str, role: str, text: str):
 
         message = {
             'chat_id': chat_id,
@@ -35,8 +38,13 @@ class HistoryBase:
 
         with self.engine.begin() as conn:
             conn.execute(self.history.insert(), [message])
+        if role == 'user':
+            logger.info("ADD MESSAGES: chat_id: %s role: %s, text: %s" % (chat_id, role, text))
+        else:
+            logger.info("ADD MESSAGES: chat_id: %s role: %s" % (chat_id, role))
         
-    def get_messages(self, chat_id: int, limit=None):
+    def get_messages(self, chat_id: str, limit=None):
+        logger.info("GET MESSAGES: chat_id: %s limit: %s" % (chat_id, limit))
         query = self.history.select().where(self.history.c.chat_id == chat_id)
         if limit is not None:
             query = query.limit(limit)
@@ -44,7 +52,8 @@ class HistoryBase:
             rows = conn.execute(query).mappings().all()
             return [dict(row) for row in rows] if rows else None
     
-    def delete_messages_by_chat_id(self, chat_id: int):
+    def delete_messages_by_chat_id(self, chat_id: str):
+        logger.info("DELETE MESSAGES: chat_id: %s" % (chat_id))
         stmt = delete(self.history).where(self.history.c.chat_id == chat_id)
         with self.engine.begin() as conn:
             conn.execute(stmt)
